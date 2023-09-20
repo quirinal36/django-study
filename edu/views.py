46a76692812ff0536acf67614d8bc42e32c42b4f
leehg <turboguy36@hotmail.com>
2023-09-20 11:15:34 +0900

    데이터베이스에 글 등록, 사진 업로드

--- edu/views.py
+++ edu/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import View
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Feed
 
 class Index(View):
     template_name = 'index.html'
@@ -11,4 +12,22 @@ class TagStudy(View):
     template_name = 'tag_study.html'
     
     def get(self, request):
+        feeds = Feed.objects.all().order_by('id')
+        
+        return render(request, self.template_name, {'feed_list':feeds})
+    
+class NewContent(View):
+    template_name = 'upload_form.html'
+    
+    def get(self, request):
         return render(request, self.template_name)
+    
+    def post(self, request):
+        param = request.POST.get('content', '')
+        param2 = request.FILES.get('up_photo', False)
+        
+        print(param)
+        
+        feed = Feed(content=param, photo = param2)
+        feed.save()
+        return redirect('edu:tag_study')
